0b79cd5f401328208a7a15ecc472b7d26c377f40
Natalia <natalia@buziaczek.pl>
2021-06-15 22:00:46 +0200

    pusia

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -35,14 +35,14 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 class ValidatorTests {
 
-	private Validator createValidator() {
+	private Validator createValidator(Pusia) {
 		LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
 		localValidatorFactoryBean.afterPropertiesSet();
 		return localValidatorFactoryBean;
 	}
 
 	@Test
-	void shouldNotValidateWhenFirstNameEmpty() {
+	void shouldNotValidateWhenFirstNameEmpty(pusia) {
 // given
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
