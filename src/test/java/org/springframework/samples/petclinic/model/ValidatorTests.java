cd760a747d641db84c84957c3727e1033c402ee4
Natalia <natalia@buziaczek.pl>
2021-06-15 22:06:48 +0200

    pusia

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -42,7 +42,7 @@ class ValidatorTests {
 	}
 
 	@Test
-	void shouldNotValidateWhenFirstNameEmpty(3 {
+	void shouldNotValidateWhenFirstNameEmpty(3) {
 // given
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
